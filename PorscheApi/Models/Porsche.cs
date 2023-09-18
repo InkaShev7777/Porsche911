7930585ee71ba23078e5d0d47469612f88f8d58e
Ilya Shevchenko <Sevc_ph43@student.itstep.org>
2023-09-18 23:46:34 +0300

    Done Scaffold(models,connext)

---
+++ PorscheApi/Models/Porsche.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PorscheApi;
+
+public partial class Porsche
+{
+    public int Id { get; set; }
+
+    public string Model { get; set; } = null!;
+
+    public string Years { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+}
