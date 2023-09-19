da0aff360e50c10fb15ff6f3218b0f99f378d46c
Ilya Shevchenko <Sevc_ph43@student.itstep.org>
2023-09-19 12:38:47 +0300

    Add request to API

--- app/src/main/java/com/example/porschecatalog/MainActivity.java
+++ app/src/main/java/com/example/porschecatalog/MainActivity.java
@@ -2,13 +2,130 @@ package com.example.porschecatalog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView porscheListView;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> porsheList;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, new TrustManager[]{new InsecureTrustManager()}, null);
+            HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        Bundle arguments = getIntent().getExtras();
+        porscheListView = findViewById(R.id.porschelist);
+        porsheList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, porsheList);
+        porscheListView.setAdapter(adapter);
+
+        new HttpGetRequest().execute("https://10.0.2.2:7067/porsche/GetPorsche","GET");
+    }
+    private class HttpGetRequest extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String apiUrl = params[0];
+
+            try {
+                URL url = new URL(apiUrl);
+                HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                    public boolean verify(String hostname, SSLSession session) {
+                        return true;
+                    }
+                });
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                if(params[1] == "GET"){
+                    connection.setRequestMethod("GET");
+                }
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+
+                    reader.close();
+                    return response.toString();
+                } else {
+                    Log.e("HTTP GET Request", "HTTP Response Code: " + responseCode);
+                    return null;
+                }
+            } catch (IOException e) {
+                Log.e("HTTP GET Request", "IOException: " + e.getMessage());
+                return null;
+            }
+        }
+        @Override
+        protected void onPostExecute(String response) {
+            if (response != null) {
+                try {
+                    JSONArray jsonArray = new JSONArray(response);
+                    porsheList.clear();
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        String title = jsonObject.getString("model");
+//                        String body = jsonObject.getString("body");
+                        porsheList.add(title);
+                    }
+                    adapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Toast.makeText(getApplicationContext(), "Произошла ошибка при получении заметок.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    public static class InsecureTrustManager implements X509TrustManager {
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return null;
+        }
     }
 }
